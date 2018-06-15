dfc043eaad734f5bfa4a47f5d043e875f1bd8eb3
Manish4586 <Manish.n.manish45@gmail.com>
2018-06-15 18:25:01 +0530

    Dissable

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
  cd ../../../..
- cd packages/apps/Settings
- patch -p1 -b < ../../../device/lenovo/a369i/patches/dev_info.patch
- git clean -f -d
- cd ../../..
+ #cd packages/apps/Settings
+ #patch -p1 -b < ../../../device/lenovo/a369i/patches/dev_info.patch
+ #git clean -f -d
+ #cd ../../..
  cd system/core
  patch -p1 < ../../device/lenovo/a369i/patches/
  surfaceflinger.patch
- patch -p1 < ../../device/lenovo/a369i/patches/
+ #patch -p1 < ../../device/lenovo/a369i/patches/
  batteryVoltage.patch
 cd ../..
 cd system/netd

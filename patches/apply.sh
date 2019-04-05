2c1ba0ee7e3c282b4108afb931cdf91e85deb955
melekpro <melekpro6@gmail.com>
2019-04-05 17:25:16 -0400

    Update apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -7,10 +7,14 @@
  cd system/core
  patch -p1 < ../../device/lenovo/a369i/patches/
  surfaceflinger.patch
+ patch -p1 < ../../device/lenovo/a369i/patches/Add-back-atomic-symbols.patch
+ patch -p1 < ../../device/lenovo/a369i/patches/Bring-back-earlysuspend.patch
  #patch -p1 < ../../device/lenovo/a369i/patches/
  batteryVoltage.patch
 cd ../..
 cd system/netd
 patch -p1 < ../../device/lenovo/a369i/patches/netd.patch
+cd frameworks/base
+patch -p1 < ../../device/lenovo/a369i/patches/Add-proc-ged-to-zygote-whitelist.patch
 cd ../..
 

d5aaefe28bc5ba56f821aa18df3bf02bf949ad90
melekpro <melekpro6@gmail.com>
2019-04-05 17:56:35 -0400

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -4,12 +4,18 @@ TARGET_SCREEN_WIDTH := 480
 
 PRODUCT_LOCALES := en_US ru_RU
 
-# Inherit some common CM stuff.
+# Inherit some common CM stuff
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Call device specific makefile
 $(call inherit-product, device/lenovo/a369i/device.mk)
 
+LOCAL_PATH := device/lenovo/a369i
 
+# Common CM stuff
+CM_BUILD := a369i
+
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_a369i
